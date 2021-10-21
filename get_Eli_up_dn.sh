8fe24bc565c871be24a510c195747738a07ad7cc
computing-bioX <87118940+computing-bioX@users.noreply.github.com>
2021-10-21 09:05:06 -0400

    Update get_Eli_up_dn.sh

--- get_Eli_up_dn.sh
+++ get_Eli_up_dn.sh
@@ -1,2 +1,3 @@
+###get the first 2 columns from ELI_scores.xlsx, then we will get test.txt
 sort -k2,2 -nr test.txt |head -n 100 |cut -f 1 >up100.txt
 sort -k2,2 -nr test.txt |tail -n 100|cut -f 1 >dn100.txt
